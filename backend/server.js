5a5de98c5aa3c52e27aa93691da29a200a974478
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-12-14 15:45:47 +0300

    connecting-with-mongo-dbcompas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -28,5 +29,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   5000,
-  console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on Port ${PORT}`.yellow.bold
+  )
 )

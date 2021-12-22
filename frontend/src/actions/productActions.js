b72d2524101a4103562fe9de23a806631b4f0e94
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-12-22 18:45:57 +0300

    list-products-action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -9,4 +11,29 @@ import {
 //now wonder there is a function inside a function
 //redux thunk helps in this
 //instead of getting products from the component, we are going to get products from this action
-export const listProducts = () => async (dispatch) => {}
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_LIST_REQUEST,
+    })
+
+    const { data } = await axios.get('/api/products')
+
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+//I can have one function then return the dispatch inside
+//or i may use the above approach

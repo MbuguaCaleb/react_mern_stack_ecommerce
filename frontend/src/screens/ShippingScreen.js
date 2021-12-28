9ebb85dbff21bebe0f632ce6912cb5c0e8e12099
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-12-28 17:58:27 +0300

    checkout-steps-wizard

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../actions/cartActions'
+import CheckOutSteps from '../components/CheckOutSteps'
 
 function ShippingScreen({ history }) {
   const cart = useSelector((state) => state.cart)
@@ -24,6 +25,7 @@ function ShippingScreen({ history }) {
   }
   return (
     <FormContainer>
+      <CheckOutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address' className='pb-3'>

c76b6b0cdd9cf0a98ac3c370569de5fb354c7e97
Mbugua Caleb <mbuguacaleb30@gmail.com>
2022-01-03 09:21:16 +0300

    get-all-users-admin-endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -31,4 +31,13 @@ const protect = asyncHandler(async (req, res, next) => {
     throw new Error('Not Authorized, no token')
   }
 })
-export { protect }
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as admin')
+  }
+}
+export { protect, admin }
